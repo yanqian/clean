167933ac4a620fcd1057433b724e023da1d166a8
yanqian <yanqiangsdu@gmail.com>
2015-12-29 22:01:20 +0800

    solve /boot not enough space when updating softwares

---
+++ clean-boot.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+kernels=$(dpkg --get-selections | grep linux-image-[0-9] | awk '{print $1}')
+counts=$(dpkg --get-selections | grep linux-image-[0-9] | wc -l)
+count=1
+for ele in $kernels
+do
+    if [ $count -lt $counts ];then
+        # keep the last newest kernel and remove others
+        sudo apt-get --purge remove $ele
+        count=$(expr $count + 1)
+    else
+        break
+    fi
+done
